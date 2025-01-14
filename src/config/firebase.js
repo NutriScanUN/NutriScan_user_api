b10353c535bb45089bcbde08cec4bd9dc772ad43
Juan Carreño <jcarrenoar@unal.edu.co>
2025-01-14 13:06:48 -0500

    Add new key firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,7 +2,7 @@ const admin = require('firebase-admin');
 const path = require('path');
 
 // Inicializar Firebase Admin SDK
-const serviceAccountPath = path.join(__dirname, '../../nutriscan-71493-firebase-adminsdk-ugmom-589ff76776.json');
+const serviceAccountPath = path.join(__dirname, '../../nutriscan-71493-firebase-adminsdk-ugmom-e4fbdbaa0b.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(require(serviceAccountPath)),
